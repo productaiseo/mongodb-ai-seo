c7f6cc455a1a4a733c709ff09b7c6878ee4088a5
yusufpirejman@gmail.com <yusufpirejman@gmail.com>
2025-09-29 01:57:56 +0300

    Add initial project setup with Next.js, Tailwind CSS, and essential configurations

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="h-screen bg-black">
+      <Button variant="default">Click me</Button>
+    </div>
+  );
+}
