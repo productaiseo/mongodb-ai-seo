6c3b3a00f51add72695264ce9452bd10431ccaf4
yusufie <yusufpirejman@gmail.com>
2025-10-22 00:22:54 +0300

    fix: Adjust padding in Hero component and reorder imports in Navbar for consistency

--- src/components/Navs/Navbar.tsx
+++ src/components/Navs/Navbar.tsx
@@ -1,8 +1,10 @@
 "use client";
 
-import Languages from "@/components/Dropdowns/Languages";
 import { useTranslations } from "next-intl";
 import Link from 'next/link';
+import { FiBarChart2, FiLayers } from 'react-icons/fi';
+
+import Languages from "@/components/Dropdowns/Languages";
 import Logo from '@/components/Logo';
 
 
@@ -13,16 +15,19 @@ const Navbar = () => {
   return (
       <div className="container flex justify-between items-center mx-auto z-10">
         <Logo variant="header" />
+
         <div className="flex items-center gap-6">
           <Link href="/features" className="text-white/80 hover:text-white transition-colors">
-            {t('NavFeatures')}
+            <span className="hidden sm:inline">{t('NavFeatures')}</span>
+            <FiBarChart2 className="sm:hidden inline h-6 w-6" />
           </Link>
+
           <Link href="/about" className="text-white/80 hover:text-white transition-colors">
-            {t('NavHowItWorks')}
+            <span className="hidden sm:inline">{t('NavHowItWorks')}</span>
+            <FiLayers className="sm:hidden inline h-6 w-6" />
           </Link>
 
           <Languages />
-
         </div>
       </div>
   );
